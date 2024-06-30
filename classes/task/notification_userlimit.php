149d5789031187ee5d822cc507eb3b439ef80911
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-06-30 16:52:05 -0500

    Update notification_userlimit.php

--- classes/task/notification_userlimit.php
+++ classes/task/notification_userlimit.php
@@ -18,23 +18,18 @@ namespace report_usage_monitor\task;
 
 defined('MOODLE_INTERNAL') || die();
 
-class notification_userlimit extends \core\task\scheduled_task
-{
-    public function get_name()
-    {
+class notification_userlimit extends \core\task\scheduled_task {
+    public function get_name() {
         return get_string('processuserlimitnotificationtask', 'report_usage_monitor');
     }
 
-    public function execute()
-    {
+    public function execute() {
         global $CFG, $DB;
         require_once($CFG->dirroot . '/report/usage_monitor/locallib.php');
-
         $this->notify_user_limit();
     }
 
-    private function notify_user_limit()
-    {
+    private function notify_user_limit() {
         global $DB;
         $reportconfig = get_config('report_usage_monitor');
         $lastday_users = user_limit_daily_sql(get_string('dateformatsql', 'report_usage_monitor'));
@@ -47,7 +42,6 @@ class notification_userlimit extends \core\task\scheduled_task
             $last_notificationusers_time = get_config('report_usage_monitor', 'last_notificationusers_time');
             $current_time = time();
 
-            // Ensure notifications are sent based on the interval
             if ($current_time - $last_notificationusers_time >= $notification_interval) {
                 email_notify_user_limit($item->conteo_accesos_unicos, $item->fecha, $users_percent);
                 set_config('last_notificationusers_time', $current_time, 'report_usage_monitor');
@@ -57,17 +51,15 @@ class notification_userlimit extends \core\task\scheduled_task
 
     private function calculate_notification_interval($users_percent) {
         $thresholds = [
-            99.9 => 12 * 60 * 60,   // 12 hours
-            98.5 => 24 * 60 * 60,   // 1 day
-            90 => 5 * 24 * 60 * 60, // 5 days
+            99.9 => 12 * 60 * 60,   // 12 horas
+            98.5 => 24 * 60 * 60,   // 1 día
+            90 => 5 * 24 * 60 * 60, // 5 días
         ];
-
         foreach ($thresholds as $threshold => $interval) {
             if ($users_percent >= $threshold) {
                 return $interval;
             }
         }
-
         return 0; // No notification if under 90%
     }
 }
