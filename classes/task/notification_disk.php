e9ee9d7224d545c47202f9c7e89b5d9000d7c28f
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-07-01 23:09:41 -0500

    Update notification_disk.php

--- classes/task/notification_disk.php
+++ classes/task/notification_disk.php
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
+
 namespace report_usage_monitor\task;
 
 defined('MOODLE_INTERNAL') || die();
@@ -30,7 +31,15 @@ class notification_disk extends \core\task\scheduled_task
         global $CFG;
         require_once($CFG->dirroot . '/report/usage_monitor/locallib.php');
 
+        if (debugging('', DEBUG_DEVELOPER)) {
+            mtrace("Iniciando tarea de notificación de uso de disco...");
+        }
+
         self::notify_disk_usage();
+
+        if (debugging('', DEBUG_DEVELOPER)) {
+            mtrace("Tarea de notificación de uso de disco completada.");
+        }
     }
 
     private static function calculate_notification_interval($disk_percent)
@@ -56,14 +65,25 @@ class notification_disk extends \core\task\scheduled_task
         $reportconfig = get_config('report_usage_monitor');
         $quotadisk = ((int) $reportconfig->disk_quota * 1024) * 1024 * 1024;
         $disk_usage = ((int) $reportconfig->totalusagereadable + (int) $reportconfig->totalusagereadabledb) ?: 0;
-        $disk_percent = ($quotadisk > 0) ? ($disk_usage / $quotadisk) * 100 : 0;
+        $disk_percent = calculate_user_threshold_percentage($disk_usage, $quotadisk);
+
+        if (debugging('', DEBUG_DEVELOPER)) {
+            mtrace("Cuota de disco: $quotadisk bytes, Uso de disco: $disk_usage bytes, Porcentaje de disco: $disk_percent%");
+        }
 
         $notification_interval = self::calculate_notification_interval($disk_percent);
         $last_notificationdisk_time = get_config('report_usage_monitor', 'last_notificationdisk_time');
         $current_time = time();
 
+        if (debugging('', DEBUG_DEVELOPER)) {
+            mtrace("Intervalo de notificación: $notification_interval segundos, Última notificación: $last_notificationdisk_time");
+        }
+
         if ($current_time - $last_notificationdisk_time >= $notification_interval) {
             $userAccessCount = self::get_total_user_access_count();
+            if (debugging('', DEBUG_DEVELOPER)) {
+                mtrace("Enviando notificación de uso de disco...");
+            }
             email_notify_disk_limit($quotadisk, $disk_usage, $disk_percent, $userAccessCount);
             set_config('last_notificationdisk_time', $current_time, 'report_usage_monitor');
         }
