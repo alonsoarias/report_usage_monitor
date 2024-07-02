26aa414089216c6c72ccbf9d8748ea235fda7807
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-07-01 23:09:50 -0500

    Update users_daily_90_days.php

--- classes/task/users_daily_90_days.php
+++ classes/task/users_daily_90_days.php
@@ -23,51 +23,44 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 o posterior
  */
 
-namespace report_usage_monitor\task;
 
-// Prevenir el acceso directo a este archivo.
-defined('MOODLE_INTERNAL') || die();
-
-/**
- * Tarea para calcular los usuarios principales en los últimos 90 días.
- *
- * @package     report_usage_monitor
- * @category    admin
- * @copyright   2023 Soporte IngeWeb <soporte@ingeweb.co>
- * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 o posterior
- */
-class users_daily_90_days extends \core\task\scheduled_task
-{
-
-    /**
-     * Obtener el nombre de la tarea tal como se muestra en las pantallas de administración.
-     *
-     * @return string
-     * @throws \coding_exception
-     */
-    public function get_name()
-    {
-        return get_string('getlastusers90days', 'report_usage_monitor');
-    }
-
-    /**
-     * Ejecutar la tarea para calcular los usuarios principales en los últimos 90 días.
-     */
-    public function execute() {
-        global $DB, $CFG;
-        require_once($CFG->dirroot . '/report/usage_monitor/locallib.php');
-    
-        // Asumiendo que la función max_userdaily_for_90_days devuelve una consulta SQL correcta.
-        $sql = max_userdaily_for_90_days(get_string('dateformatsql', 'report_usage_monitor'));
-        $users_90_days_records = $DB->get_records_sql($sql);
-        foreach ($users_90_days_records as $record) {
-            // Asegúrate de que el nombre de la columna en tu consulta SQL sea 'usuarios'.
-            // Si no, reemplaza 'usuarios' con el nombre de columna correcto.
-            if (isset($record->usuarios)) {
-                set_config('max_userdaily_for_90_days_date', $record->fecha, 'report_usage_monitor');
-                set_config('max_userdaily_for_90_days_users', $record->usuarios, 'report_usage_monitor');
-            }
-        }
-    }
-    
-}
+ namespace report_usage_monitor\task;
+ 
+ defined('MOODLE_INTERNAL') || die();
+ 
+ /**
+  * Tarea para calcular los usuarios principales en los últimos 90 días.
+  */
+ class users_daily_90_days extends \core\task\scheduled_task
+ {
+     public function get_name()
+     {
+         return get_string('getlastusers90days', 'report_usage_monitor');
+     }
+ 
+     public function execute() {
+         global $DB, $CFG;
+         require_once($CFG->dirroot . '/report/usage_monitor/locallib.php');
+ 
+         if (debugging('', DEBUG_DEVELOPER)) {
+             mtrace("Iniciando tarea de cálculo de usuarios diarios en los últimos 90 días...");
+         }
+ 
+         // Asumiendo que la función max_userdaily_for_90_days devuelve una consulta SQL correcta.
+         $sql = max_userdaily_for_90_days(get_string('dateformatsql', 'report_usage_monitor'));
+         $users_90_days_records = $DB->get_records_sql($sql);
+         foreach ($users_90_days_records as $record) {
+             // Asegúrate de que el nombre de la columna en tu consulta SQL sea 'usuarios'.
+             if (isset($record->usuarios)) {
+                 set_config('max_userdaily_for_90_days_date', $record->fecha, 'report_usage_monitor');
+                 set_config('max_userdaily_for_90_days_users', $record->usuarios, 'report_usage_monitor');
+             }
+         }
+ 
+         if (debugging('', DEBUG_DEVELOPER)) {
+             mtrace("Usuarios principales en los últimos 90 días calculados.");
+             mtrace("Tarea de cálculo de usuarios principales en los últimos 90 días completada.");
+         }
+     }
+ }
+ 
