a5716f115888024401b29df5a911404231cdb145
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-01-05 07:48:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
-//
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -10,16 +9,14 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Usage Monitor Report
+ * Página principal del reporte "Usage Monitor".
  *
- * @package    report_usage_monitor
- * @copyright  2023 Soporte IngeWeb <soporte@ingeweb.co>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     report_usage_monitor
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require_once('../../config.php');
@@ -27,178 +24,231 @@ require_once($CFG->libdir . '/adminlib.php');
 require_once($CFG->dirroot . '/report/usage_monitor/locallib.php');
 
 admin_externalpage_setup('report_usage_monitor', '', null, '', ['pagelayout' => 'report']);
-$viewtab = optional_param('view', 'usage_monitor', PARAM_ALPHA);
 
-// Datos para las pestañas en el informe.
-$tabdata = ['userstopnum' => '', 'diskusage' => ''];
+// Pestaña principal
+$viewtab = optional_param('view', 'usage_monitor', PARAM_ALPHA);
+// Subpestaña para la sección de usuarios
+$subview = optional_param('subview', 'usertable', PARAM_ALPHA);
+
+// Definimos las pestañas principales
+$tabdata = [
+    'userstopnum' => '',
+    'diskusage'   => ''
+];
+
+// Validar la pestaña
 if (!array_key_exists($viewtab, $tabdata)) {
-    // Para un valor de parámetro inválido, utilizar 'USAGE_MONITOR'.
     $viewtab = array_keys($tabdata)[0];
 }
 
+// Construir las pestañas de nivel superior
 $tabs = [];
-foreach ($tabdata as $tabname => $param) {
+foreach ($tabdata as $tabname => $unused) {
     $tabs[] = new tabobject(
         $tabname,
         new moodle_url($PAGE->url, ['view' => $tabname]),
-        get_string($tabname, 'report_usage_monitor', $param)
+        get_string($tabname, 'report_usage_monitor')
     );
 }
-if (empty($download)) {
-    print $OUTPUT->header();
-    echo $OUTPUT->tabtree($tabs, $viewtab);
-}
+
+// Imprimir header y tabs
+echo $OUTPUT->header();
+echo $OUTPUT->tabtree($tabs, $viewtab);
+
+// Configuración del plugin
 $reportconfig = get_config('report_usage_monitor');
-if ($viewtab == 'userstopnum') {
-    $updatestring = !empty($reportconfig->lastexecution) ? userdate($reportconfig->lastexecution) : get_string('notcalculatedyet', 'report_usage_monitor');
-    $updatestring2 = !empty($reportconfig->totalusersdaily) ? ($reportconfig->totalusersdaily) : get_string('notcalculatedyet', 'report_usage_monitor');
-    echo html_writer::span(get_string('lastexecution', 'report_usage_monitor', $updatestring));
-    echo "<br/>";
-    echo html_writer::span(get_string('users_today', 'report_usage_monitor', $updatestring2));
-    echo $OUTPUT->heading(get_string('max_userdaily_for_90_days', 'report_usage_monitor'));
-    $table2 = new html_table();
-    $table2->colclasses[] = 'centeralign';
-    $table2->attributes['cellpadding'] = '0';
-    $table2->head = array(
-        get_string('date', 'report_usage_monitor'),
-        get_string('usersquantity', 'report_usage_monitor')
-    );
-    $updatestring3 = !empty($reportconfig->max_userdaily_for_90_days_date) ? date(get_string('dateformat', 'report_usage_monitor'), $reportconfig->max_userdaily_for_90_days_date) : get_string('notcalculatedyet', 'report_usage_monitor');
-    $updatestring4 = !empty($reportconfig->max_userdaily_for_90_days_users) ? ($reportconfig->max_userdaily_for_90_days_users) : get_string('notcalculatedyet', 'report_usage_monitor');
-    $table2->data[] =  array(($updatestring3), ($updatestring4));
-    echo html_writer::table($table2);
-    // Código para mostrar la subpestaña con opciones de visualización
 
-    $subtabs = array(
-        new tabobject('usertable', new moodle_url($PAGE->url, ['view' => 'userstopnum', 'subview' => 'usertable']), get_string('usertable', 'report_usage_monitor')),
-        new tabobject('userchart', new moodle_url($PAGE->url, ['view' => 'userstopnum', 'subview' => 'userchart']), get_string('userchart', 'report_usage_monitor'))
-    );
-    echo $OUTPUT->heading(get_string('topuser', 'report_usage_monitor'));
-    $subtabtree = $OUTPUT->tabtree($subtabs, optional_param('subview', 'usertable', PARAM_ALPHA));
+// -------------------------------------------------------------------
+// PESTAÑA: USUARIOS DIARIOS (userstopnum)
+// -------------------------------------------------------------------
+if ($viewtab === 'userstopnum') {
+    // Título principal
+    echo $OUTPUT->heading(get_string('userstopnum', 'report_usage_monitor'));
 
-    if ($subtabtree) {
-        echo $subtabtree;
+    // Texto de alerta
+    echo html_writer::tag(
+        'div',
+        get_string('exclusivedisclaimer', 'report_usage_monitor'),
+        ['class' => 'alert alert-warning']
+    );
+
+    // ---------------------------------------------------------------
+    // SUBTABS: usertable, userchart (para los últimos 10 días)
+    // ---------------------------------------------------------------
+    $subtabs = [
+        new tabobject(
+            'usertable',
+            new moodle_url($PAGE->url, ['view' => 'userstopnum', 'subview' => 'usertable']),
+            get_string('usertable', 'report_usage_monitor')
+        ),
+        new tabobject(
+            'userchart',
+            new moodle_url($PAGE->url, ['view' => 'userstopnum', 'subview' => 'userchart']),
+            get_string('userchart', 'report_usage_monitor')
+        ),
+    ];
+
+    // Mostrar subtabs
+    echo $OUTPUT->tabtree($subtabs, $subview);
+
+    // ---------------------------------------------------------------
+    // 1) Sección de "últimos 10 días" (tabla o gráfica según $subview)
+    // ---------------------------------------------------------------
+    // Consulta para los últimos 10 días
+    $last10sql     = report_user_daily_sql(get_string('dateformatsql', 'report_usage_monitor'));
+    $last10records = $DB->get_records_sql($last10sql);
+
+    // Preparar arrays para tabla / gráfica
+    $labelsLast10 = [];
+    $dataLast10   = [];
+
+    foreach ($last10records as $r) {
+        // Almacenar para potencial gráfica
+        $labelsLast10[] = $r->fecha;
+        $dataLast10[]   = (int)$r->conteo_accesos_unicos;
     }
 
-    if (optional_param('subview', 'usertable', PARAM_ALPHA) == 'usertable') {
-        $table1 = new html_table();
-        $table1->head = array(
+    // Si subview = usertable => tabla
+    if ($subview === 'usertable') {
+        echo $OUTPUT->heading(get_string('lastusers', 'report_usage_monitor'));
+        $table_last10 = new html_table();
+        $table_last10->head = [
             get_string('date', 'report_usage_monitor'),
             get_string('usersquantity', 'report_usage_monitor')
-        );
-        $table1->colclasses[] = 'centeralign';
-        $table1->attributes['cellpadding'] = '0';
-        $userdailytop = report_user_daily_top_sql(get_string('dateformatsql', 'report_usage_monitor'));
-        $userdaily_recordstop = $DB->get_records_sql($userdailytop);
-        foreach ($userdaily_recordstop as $log) {
-            $table1->data[] = array(
-                $log->fecha,
-                $log->cantidad_usuarios
-            );
+        ];
+        foreach ($last10records as $row) {
+            $table_last10->data[] = [
+                $row->fecha,
+                $row->conteo_accesos_unicos
+            ];
         }
-        echo html_writer::table($table1);
-    } elseif (optional_param('subview', 'usertable', PARAM_ALPHA) == 'userchart') {
-        $chart = new \core\chart_line();
-        $chart->set_smooth(true);
-        $userdailytop = report_user_daily_top_sql(get_string('dateformatsql', 'report_usage_monitor'));
-        $userdaily_recordstop = $DB->get_records_sql($userdailytop);
-        $data = [];
-        foreach ($userdaily_recordstop as $log) {
-            // Eliminada la línea problemática que intentaba añadir datos a $table->data.
-            $data[$log->fecha] = $log->cantidad_usuarios;
-        }
-        uksort($data, 'compararFechas');
-        $chart->set_labels(array_keys($data)); // Fechas
-        $series = new \core\chart_series(
-            get_string('usersquantity', 'report_usage_monitor'),
-            array_values($data)
-        );
-        $chart->add_series($series);
-    
-        // Mostrar la gráfica
-        echo $OUTPUT->render($chart);
-    }
-    
-    echo $OUTPUT->heading(get_string('lastusers', 'report_usage_monitor'));
-    $table = new html_table();
-    $table->head = array(
-        get_string('date', 'report_usage_monitor'),
-        get_string('usersquantity', 'report_usage_monitor'),
-    );
-    $table->colclasses[] = 'centeralign';
-    $table->attributes['cellpadding'] = '0';
-    $userdaily = report_user_daily_sql(get_string('dateformatsql', 'report_usage_monitor'));
-    $userdaily_records = $DB->get_records_sql($userdaily);
-    foreach ($userdaily_records as $log) {
-        $table->data[] = array(
-            $log->fecha,
-            $log->conteo_accesos_unicos
-        );
-    }
-    echo html_writer::table($table);
-} elseif ($viewtab == 'diskusage') {
-    echo $OUTPUT->heading(get_string('diskusage', 'report_usage_monitor'));
-    $updatestring5 = !empty($reportconfig->lastexecutioncalculate) ? userdate($reportconfig->lastexecutioncalculate) : get_string('notcalculatedyet', 'report_usage_monitor');
-    $updatestring6 = !empty($reportconfig->totalusagereadable) ? ($reportconfig->totalusagereadable) : get_string('notcalculatedyet', 'report_usage_monitor');
-    $updatestring7 = !empty($reportconfig->totalusagereadabledb) ? ($reportconfig->totalusagereadabledb) : get_string('notcalculatedyet', 'report_usage_monitor');
-    echo html_writer::span(get_string('lastexecutioncalculate', 'report_usage_monitor', $updatestring5));
+        echo html_writer::table($table_last10);
 
-    $table3 = new html_table();
-    $table3->colclasses[] = 'centeralign';
-    $table3->attributes['cellpadding'] = '0';
-    $table3->head = array(
+    // Si subview = userchart => gráfica
+    } elseif ($subview === 'userchart') {
+        echo $OUTPUT->heading(get_string('lastusers', 'report_usage_monitor'));
+        $chartLast10 = new \core\chart_line();
+        $chartLast10->set_smooth(true);
+        $chartLast10->set_labels($labelsLast10);
+
+        $seriesLast10 = new \core\chart_series(
+            get_string('usersquantity', 'report_usage_monitor'),
+            $dataLast10
+        );
+        $chartLast10->add_series($seriesLast10);
+        $chartLast10->set_title(get_string('lastusers', 'report_usage_monitor'));
+
+        echo $OUTPUT->render($chartLast10);
+    }
+
+    // ---------------------------------------------------------------
+    // 2) Sección "Top 10 Usuarios"
+    // ---------------------------------------------------------------
+    echo $OUTPUT->heading(get_string('topuser', 'report_usage_monitor'));
+
+    // Consulta del top de usuarios guardado en la tabla local
+    $topsql     = report_user_daily_top_sql(get_string('dateformat', 'report_usage_monitor'));
+    $toprecords = $DB->get_records_sql($topsql);
+
+    $table_top = new html_table();
+    $table_top->head = [
+        get_string('date', 'report_usage_monitor'),
+        get_string('usersquantity', 'report_usage_monitor')
+    ];
+    if (!empty($toprecords)) {
+        foreach ($toprecords as $row) {
+            $table_top->data[] = [
+                $row->fecha,
+                $row->cantidad_usuarios
+            ];
+        }
+    }
+    echo html_writer::table($table_top);
+
+// -------------------------------------------------------------------
+// PESTAÑA: USO DE DISCO (diskusage)
+// -------------------------------------------------------------------
+} elseif ($viewtab === 'diskusage') {
+    echo $OUTPUT->heading(get_string('diskusage', 'report_usage_monitor'));
+
+    echo html_writer::tag(
+        'div',
+        get_string('exclusivedisclaimer', 'report_usage_monitor'),
+        ['class' => 'alert alert-warning']
+    );
+
+    // Último cálculo del uso de disco
+    $updatestringcalculate = !empty($reportconfig->lastexecutioncalculate)
+        ? userdate($reportconfig->lastexecutioncalculate)
+        : get_string('notcalculatedyet', 'report_usage_monitor');
+
+    echo html_writer::span(
+        get_string('lastexecutioncalculate', 'report_usage_monitor', $updatestringcalculate)
+    );
+
+    // Calcular el uso real
+    $usedFS  = (float)($reportconfig->totalusagereadable   ?? 0);
+    $usedDB  = (float)($reportconfig->totalusagereadabledb ?? 0);
+    $usedTotalBytes = $usedFS + $usedDB;
+
+    // Cuota en GB -> bytes
+    $disk_quota_gb    = (float)get_config('report_usage_monitor', 'disk_quota');
+    $disk_quota_bytes = $disk_quota_gb * 1024 * 1024 * 1024;
+
+    // Convertir a string legible
+    $usedTotalStr = display_size($usedTotalBytes);
+    $dbSizeStr    = display_size($usedDB);
+
+    // Tabla con info de disco
+    $table_disk = new html_table();
+    $table_disk->head = [
         get_string('sizeusage', 'report_usage_monitor'),
         get_string('avalilabledisk', 'report_usage_monitor'),
         get_string('sizedatabase', 'report_usage_monitor')
-    );
+    ];
 
-    // Convertir los tamaños a GB para la tabla y agregar la etiqueta "GB"
-    $used_space_gb_db = display_size_in_gb((float)$updatestring7, 2); // Asegurándose de que sea un valor flotante
-    $used_space_total = display_size_in_gb((floatval($updatestring6) + floatval($updatestring7)), 2);    
-    // Obtener la cuota de disco desde la configuración
-    $disk_quota_gb = get_config('report_usage_monitor', 'disk_quota');
-    $free_space = $disk_quota_gb - $used_space_total;
-    $free_space_percentage = round((($free_space / $disk_quota_gb) * 100),2) . '%';
-    $table3->data[] = array($used_space_total . ' GB / ' . $disk_quota_gb . ' GB',  $free_space_percentage, $used_space_gb_db.' GB');
-    echo html_writer::table($table3);
-
-    // Crear la gráfica de barras horizontales del espacio en disco usado y el espacio restante con el límite
-    echo $OUTPUT->heading(get_string('diskusage', 'report_usage_monitor'));
-    $chart = new \core\chart_bar();
-    $chart->set_horizontal(true);
-    $chart->set_labels(array(get_string('sizeusage', 'report_usage_monitor') . ' (GB)', get_string('sizedatabase', 'report_usage_monitor') . ' (GB)')); // Agregar la etiqueta "GB" a las leyendas
-
-    // Convertir los tamaños a GB para la gráfica
-    $used_space_total_gb = display_size_in_gb((floatval($updatestring6) + floatval($updatestring7)), 2);
-    $used_space_gb_db = display_size_in_gb($updatestring7, 2);
-
-
-    //var_dump($disk_quota_gb);
-    $total_disk_space_gb = $disk_quota_gb; // Ya estamos trabajando con GB
-
-    // Calcular los porcentajes y colores de uso del espacio en disco
-    $disk_usage = diskUsagePercentages($used_space_total_gb, $total_disk_space_gb);
-    $disk_usage_db = diskUsagePercentages($used_space_gb_db, $total_disk_space_gb);
-    // Verificar que $chart no sea null antes de configurar los colores
-    if ($chart) {
-        // Establecer los colores de las barras de la gráfica
-        $series = new \core\chart_series(get_string('diskusage', 'report_usage_monitor'), array($used_space_total_gb, $used_space_gb_db));
-        $series->set_colors(array($disk_usage['color'], $disk_usage_db['color']));
-
-        // Pasar los porcentajes de uso del espacio en disco al código JavaScript
-        echo '<script>var diskUsagePercentages = {' .
-            'folder: ' . json_encode($disk_usage) . ', ' .
-            'db: ' . json_encode($disk_usage_db) .
-            '};</script>';
-
-        // Agregar la serie a la gráfica y mostrarla
-        $chart->add_series($series);
-        echo $OUTPUT->render($chart);
-    } else {
-        echo 'Error: No se pudo crear la gráfica.';
+    // Calcular % uso
+    $usedPercent = 0;
+    if ($disk_quota_bytes > 0) {
+        $usedPercent = ($usedTotalBytes / $disk_quota_bytes) * 100;
     }
+    $freePercent = 100 - $usedPercent;
+
+    // Fila en la tabla
+    $table_disk->data[] = [
+        $usedTotalStr . ' / ' . $disk_quota_gb . ' GB',
+        round($freePercent, 2) . '%',
+        $dbSizeStr
+    ];
+    echo html_writer::table($table_disk);
+
+    // Gráfico pastel (pie)
+    $chartPie = new \core\chart_pie();
+    $chartPie->set_title(get_string('diskusage', 'report_usage_monitor'));
+
+    // Cálculo en GB
+    $usedGB = round($usedTotalBytes / (1024 * 1024 * 1024), 2);
+    $freeGB = $disk_quota_gb - $usedGB;
+    if ($freeGB < 0) {
+        $freeGB = 0;
+    }
+
+    $labelsPie = [
+        get_string('sizeusage', 'report_usage_monitor') . " ({$usedGB} GB)",
+        get_string('avalilabledisk', 'report_usage_monitor') . " ({$freeGB} GB)"
+    ];
+    $dataPie = [$usedGB, $freeGB];
+
+    $seriesPie = new \core\chart_series(
+        get_string('diskusage', 'report_usage_monitor'),
+        $dataPie
+    );
+    $chartPie->add_series($seriesPie);
+    $chartPie->set_labels($labelsPie);
+
+    echo $OUTPUT->render($chartPie);
 }
 
-//créditos
+// Créditos finales
 echo get_string('reportinfotext', 'report_usage_monitor');
 echo $OUTPUT->footer();
