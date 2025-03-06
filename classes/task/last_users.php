230b8d9e702b48902e2b390ed06818efe242d6e9
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-03-06 18:46:57 -0500

    Update last_users.php

--- classes/task/last_users.php
+++ classes/task/last_users.php
@@ -20,46 +20,55 @@
  * @package     report_usage_monitor
  * @category    admin
  * @copyright   2023 Soporte IngeWeb <soporte@ingeweb.co>
- * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 o posterior
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 
- namespace report_usage_monitor\task;
- 
- defined('MOODLE_INTERNAL') || die();
- 
- /**
-  * Tarea para calcular los usuarios conectados recientemente.
-  */
- class last_users extends \core\task\scheduled_task
- {
-     public function get_name()
-     {
-         return get_string('getlastusersconnected', 'report_usage_monitor');
-     }
- 
-     public function execute()
-     {
-         global $DB, $CFG;
-         require_once($CFG->dirroot . '/report/usage_monitor/locallib.php');
- 
-         if (debugging('', DEBUG_DEVELOPER)) {
-             mtrace("Iniciando tarea de cálculo de usuarios conectados recientemente...");
-         }
- 
-         // Recuperar los usuarios conectados recientemente para hoy.
-         $users = $DB->get_records_sql(users_today());
-         foreach ($users as $log) {
-             $users_today = $log->conteo_accesos_unicos;
-             set_config('totalusersdaily', $users_today, 'report_usage_monitor');
-         }
- 
-         if (debugging('', DEBUG_DEVELOPER)) {
-             mtrace("Usuarios conectados recientemente: $users_today.");
-             mtrace("Tarea de cálculo de usuarios conectados recientemente completada.");
-         }
- 
-         return true;
-     }
- }
- 
+namespace report_usage_monitor\task;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Tarea para calcular los usuarios conectados recientemente.
+ */
+class last_users extends \core\task\scheduled_task
+{
+    public function get_name()
+    {
+        return get_string('getlastusersconnected', 'report_usage_monitor');
+    }
+
+    public function execute()
+    {
+        global $DB, $CFG;
+        require_once($CFG->dirroot . '/report/usage_monitor/locallib.php');
+
+        if (debugging('', DEBUG_DEVELOPER)) {
+            mtrace("Iniciando tarea de cálculo de usuarios conectados recientemente...");
+        }
+
+        // Recuperar los usuarios conectados recientemente para hoy.
+        $users = $DB->get_records_sql(users_today());
+        foreach ($users as $log) {
+            $users_today = $log->conteo_accesos_unicos;
+            set_config('totalusersdaily', $users_today, 'report_usage_monitor');
+        }
+
+        // Precomputar porcentajes para el dashboard
+        $reportconfig = get_config('report_usage_monitor');
+        $max_users_threshold = (int)($reportconfig->max_daily_users_threshold ?? 100);
+        $users_percent = calculate_threshold_percentage($users_today, $max_users_threshold);
+        $users_warning_class = ($users_percent < 70) ? 'bg-success' : (($users_percent < 90) ? 'bg-warning' : 'bg-danger');
+
+        // Guardar valores precomputados
+        set_config('users_percent', $users_percent, 'report_usage_monitor');
+        set_config('users_warning_class', $users_warning_class, 'report_usage_monitor');
+
+        if (debugging('', DEBUG_DEVELOPER)) {
+            mtrace("Usuarios conectados recientemente: $users_today.");
+            mtrace("Tarea de cálculo de usuarios conectados recientemente completada.");
+        }
+
+        return true;
+    }
+}
